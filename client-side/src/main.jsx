26272ac9009dc8bd22e7afdc020b290dc6085ce5
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-06-06 16:16:35 -0300

    Removed React Devtools

--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -34,7 +34,6 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}> 
-    <ReactQueryDevtools />
     <RouterProvider router={router} />
   </QueryClientProvider>
 )
